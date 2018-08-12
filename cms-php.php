d18e4fe97a9d4f6d909380d525e1e497372c4e86
your github.com username <kandeel.a1@gmail.com>
2018-08-11 21:37:51 -0400

    first commit. create file with empty functions

---
+++ cms-php.php
@@ -0,0 +1,35 @@
+<?php
+//create class that will create database
+//connect to a database
+//display a form
+//save the form
+//display the saved data from the database
+
+class cms21iL {
+  var $host;
+  var $username;
+  var $password;
+  var $table;
+
+  public function display_public() {
+
+  }
+
+  public function display_admin() {
+
+  }
+
+  public function write() {
+
+  }
+
+  public function connect() {
+
+  }
+
+  private funtion builDB() {
+
+  }
+}
+
+ ?>
