b7d5f2a7520562c2e73a03f1775458f4ea55c889
your github.com username <kandeel.a1@gmail.com>
2018-08-13 21:35:13 -0400

    create include tag for folder

---
+++ cmsClass/display.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <title>Cms21iLabs</title>
+  </head>
+
+  <body>
+    <?php
+      include_once('cmsClass/cms.php');
+     ?>
+  </body>
+
+
+</html>
